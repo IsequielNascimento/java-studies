f502c62f5de52e8727a05652a9072e7ce4fa989b
IsequielNascimento <isequielnascimento@gmail.com>
2024-06-18 18:46:49 -0300

    Object Orientation Test

--- PrimeiraClasse/src/primeiraClasse/Socorro.java
+++ PrimeiraClasse/src/primeiraClasse/Socorro.java
@@ -1,51 +1,12 @@
 package primeiraClasse;
+
 public class Socorro {
-    private Double velocidade;
-    private String modelo;
-    private MecanismoAceleracao mecanismoAceleracao;
-    private String cor;
 
-    /* Repare que o mecanismo de aceleração é inserido no carro ao ser construído, e
-        não o vemos nem podemos modificá-lo, isto é, não tem getter nem setter.
-        Já o "modelo" pode ser visto, mas não alterado. */
-    public Socorro(String modelo, MecanismoAceleracao mecanismoAceleracao) {
-        this.modelo = modelo;
-        this.mecanismoAceleracao = mecanismoAceleracao;
-        this.velocidade = 0;
-    }
+public static void main(String[] args) {
+		
+		System.out.println("TESTE AAAAAAAAA. Hello World Padrão");
 
-    public void acelerar() {
-        this.mecanismoAceleracao.acelerar();
-    }
+	}
 
-    public void frear() {
-        /* código do carro para frear */
-    }
 
-    public void acenderFarol() {
-        /* código do carro para acender o farol */
-    }
-
-    public Double getVelocidade() {
-        return this.velocidade
-    }
-
-    private void setVelocidade() {
-        /* código para alterar a velocidade do carro */
-        /* Como só o próprio carro deve calcular a velocidade, 
-            esse método não pode ser chamado de fora, por isso é "private" */
-    }
-
-    public String getModelo() {
-        return this.modelo;
-    }
-
-    public String getCor() {
-        return this.cor;
-    }
-
-    /* podemos mudar a cor do carro quando quisermos */
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
 }

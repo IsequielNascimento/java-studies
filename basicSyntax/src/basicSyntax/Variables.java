d3461215832e8c49c2593f087b34af9410091ff6
IsequielNascimento <isequielnascimento@gmail.com>
2024-06-20 08:50:06 -0300

    Add basicSyntax package

---
+++ basicSyntax/src/basicSyntax/Variables.java
@@ -0,0 +1,22 @@
+package basicSyntax;
+
+//Are the same as C, C++, C#
+
+public class Variables {
+
+	
+	int tigrinhoGame = 4444;
+	float flutuante = 6.9f;
+	char cara = 'A';
+	boolean confias = false;
+	
+	
+
+	public static void main(String[] args) {
+		String confia = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		
+		System.out.println(confia);
+
+	}
+
+}

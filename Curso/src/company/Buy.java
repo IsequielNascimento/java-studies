00be11cc18dbf51ea11066ac75aa54aafb89cc95
IsequielNascimento <isequielnascimento@gmail.com>
2024-06-23 10:34:04 -0300

    Feat: creating new constructors to study.

---
+++ Curso/src/company/Buy.java
@@ -0,0 +1,38 @@
+package company;
+
+public class Buy {
+	
+	int totalValue;
+	int  numberInstallments;
+	
+	//In cash constructor
+	public Buy(int value) {
+		
+		totalValue = value;
+		numberInstallments = 1;
+		
+	}
+	
+	//Installments
+	
+	public Buy(int numInstallments, int valueInstallments) {
+		numberInstallments = numInstallments;
+		totalValue = valueInstallments * numInstallments;
+		
+	}
+	
+	public int getTotalValue() {
+		return totalValue;
+	}
+	
+	public int getNumInstallments() {
+		
+		return numberInstallments;
+		
+	}
+	
+	public int getValueInstallments() {
+		return totalValue / numberInstallments;
+	}
+
+}

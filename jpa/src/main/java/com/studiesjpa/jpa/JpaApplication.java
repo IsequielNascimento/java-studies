938936a11480ad88b425a31ae31d75234e2dedd5
IsequielNascimento <isequielnascimento@gmail.com>
2024-06-25 10:56:43 -0300

    updates jpaApplication and application.yaml

--- jpa/src/main/java/com/studiesjpa/jpa/JpaApplication.java
+++ jpa/src/main/java/com/studiesjpa/jpa/JpaApplication.java
@@ -2,8 +2,9 @@ package com.studiesjpa.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
